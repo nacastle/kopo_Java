4c9bc8c22a3cbe168b191b2c77c13552636024ba
nacastle <nsj3391@gmail.com>
2020-07-30 10:04:22 +0900

    오랜만에 업로드

---
+++ src/homework/day02/test.java
@@ -0,0 +1,16 @@
+package homework.day02;
+
+public class test {
+
+    private int sum = 0;
+
+
+
+    public int getSum(int a, int b) {
+        int sum = 0;
+
+        sum = a + b;
+
+        return sum;
+    }
+}
